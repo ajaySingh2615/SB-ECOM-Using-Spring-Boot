66177b318af1ae27d4408c30b1cd318460763ae3
ajaySingh2615 <ajaysingh261526@gmail.com>
2024-09-13 22:51:45 +0530

    setting up categories service

--- src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
+++ src/main/java/com/ecommerce/sb_ecom/controller/CategoryController.java
@@ -1,27 +1,30 @@
 package com.ecommerce.sb_ecom.controller;
 
 import com.ecommerce.sb_ecom.model.Category;
+import com.ecommerce.sb_ecom.service.CategoryService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class CategoryController {
+    private final CategoryService categoryService;
 
-    private final List<Category> categories = new ArrayList<Category>();
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/admin/categories")
     public String createCategory(@RequestBody Category category) {
-        categories.add(category);
+        categoryService.createCategory(category);
         return "category created";
     }
 }
---
+++ src/main/java/com/ecommerce/sb_ecom/service/CategoryService.java
@@ -0,0 +1,13 @@
+package com.ecommerce.sb_ecom.service;
+
+import com.ecommerce.sb_ecom.model.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CategoryService {
+
+    List<Category> getAllCategories();
+    void createCategory(Category category);
+}
